d9017e72131cb285f6585022c5f5d711a99c7703
ghansham01 <bindumadhavshrotriy@gmail.com>
2025-07-15 23:39:45 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import './App.css'
 import Hero from './components/hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Features from './components/Features'
+import Story from './components/Story'
+import Contect from './components/Contect'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <main className=' relative min-h-screen w-screen overflow-x-hidden bg-gray-600'>
+    <main className=' relative min-h-screen w-screen overflow-x-hidden'>
       <Navbar/>
       <Hero/>
       <About/>
+      <Features/>
+      <Story/>
+      <Contect/>
+      <Footer/>
     </main>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import Button from "./Button"
+import Animatedtital from './Animatedtital'
+
+const Footer = () => {
+  return (
+    <footer class="w-full bg-gray-900">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="py-7 border-t border-gray-700">
+          <div class="flex items-center justify-center">
+            <span class="text-gray-400 ">@ viom All rights </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
